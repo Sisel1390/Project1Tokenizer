c508b418aa261d89b02b5274051e83e88bf29c7f
Sisel1390 <lam.selena1390@gmail.com>
2024-10-10 12:44:43 -0700

    instead of manually putting in the code, just run it and type what text file you want to run

--- project1.py
+++ project1.py
@@ -1,6 +1,7 @@
 import re
 import functions
-filename = "example1.txt"
+
+filename = input("Enter which example you'd like to test: (example1.txt or example2.txt): ") # the user can run it and decide from which example text file to run
 text= functions.no_comments(filename) #makes the file easier to call in functions under a different name.
 nw = functions.no_whitespace(filename) # same thing but this is with no whitespaces compared to no comments from above
 print(functions.no_comments(filename))
